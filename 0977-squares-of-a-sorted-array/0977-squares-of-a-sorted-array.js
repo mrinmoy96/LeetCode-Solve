3d3ecb8ca81abc6a7f9d1efe1e1ad618b4d1a873
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2024-03-02 07:36:30 +0530

    Time: 86 ms (56.30%), Space: 56.6 MB (27.55%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,12 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+//
+// var sortedSquares = function(nums) {
+//     let ans=[];
+//     for(let i=0; i<nums.length; i++){
+//         let val=nums[i]*nums[i];
+//         ans.push(val);
+//     }
+//     ans.sort((a, b) => a - b);
+//     return ans;
+// };
+
 var sortedSquares = function(nums) {
-    let ans=[];
-    for(let i=0; i<nums.length; i++){
-        let val=nums[i]*nums[i];
-        ans.push(val);
-    }
-    ans.sort((a, b) => a - b);
-    return ans;
+    const squares = nums.map(num => num * num);
+    squares.sort((a, b) => a - b);
+    return squares;
 };
